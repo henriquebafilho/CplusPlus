bd6cc45c3774166854997e0c31d49e2c050483ee
Henrique Filho <hbaf71@gmail.com>
2017-01-03 03:44:06 -0200

    Calculadora
    Bug: O resultado sempre é 0.

--- main.cpp
+++ main.cpp
@@ -5,21 +5,42 @@ using namespace std;
 
 int main()
 {
+    double num;
+    double total;
+    char operacao;
+
+    num = 0;
+    total = 0;
+    operacao = '+';
+
+    while(operacao != '=')
+    {
     //OPERADOR DE EXTRAÇÃO
     cout << "Informe um numero: " << endl;
+    cin >> num;
 
-    int num1 = 0;
-    cin >> num1;
-
-    cout << "Informe outro numero: " << endl;
-    int num2 = 0;
-    cin >> num2;
-
-    cout << "Os numeros digitados foram: "
-            << num1
-            << " e "
-            << num2
-            << endl;
-
+        switch(operacao)
+        {
+            case 1 :
+                operacao = '+';
+                total = total + num;
+                break;
+            case 2 :
+                operacao = '-';
+                total = total - num;
+                break;
+            case 3 :
+                operacao = '*';
+                total = total * num;
+                break;
+            case 4 :
+                operacao = '/';
+                total = total / num;
+                break;
+        }
+     cout << "Informe a operacao: " << endl;
+     cin >> operacao;
+    }
+    cout << "O resultado eh: " << total << endl;
     return 0;
 }
