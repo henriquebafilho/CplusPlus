ac83714340692eae9dc2e6cb07454efab59803ed
Henrique Filho <hbaf71@gmail.com>
2017-01-03 20:29:28 -0200

    Calculadora de Média Escolar 2.1

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ int main()
 
     cout << "A sua media no semestre foi :" << soma << endl << endl;
 
-    if(soma > 7)
+    if(soma >= 7)
     {
         cout << "Parabens! Voce foi aprovado" << endl;
     }
