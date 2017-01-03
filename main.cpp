01d1bf13b99325daab9ab510e7d404d803595b7a
Henrique Filho <hbaf71@gmail.com>
2017-01-03 15:43:55 -0200

    Booleanas

--- main.cpp
+++ main.cpp
@@ -5,24 +5,20 @@ using namespace std;
 
 int main()
 {
-    int a = 5 / 2;
-    int num1, num2;
-    num1 = 10;
-    num2 = 3;
+    bool b = true;
+    bool b2 = (1 == 1);
 
-    int div = num1 / num2;
-    int resto = num1 % num2;
+    cout << "O valor contido em b eh: " << b << endl;
+    cout << "O valor contido em b2 eh: " << b2 << endl;
 
-    cout << "A divisao entre " << num1 << " e " << num2 << " eh igual a " << div << endl;
-    cout << "A divisao entre " << num1 << " e " << num2 << " tem resto " << resto << endl;
+    //True representa o valor 1
+    //False representa o valor 0
 
-    cout << "O modulo entre 3 e 2 eh: " << 3 % 2 << endl;
-    cout << "O modulo entre 4 e 2 eh: " << 4 % 2 << endl;
-    cout << "O modulo entre 5 e 2 eh: " << 5 % 2 << endl;
-    cout << "O modulo entre 6 e 2 eh: " << 6 % 2 << endl;
+    bool c = 0; //false
+    bool c2 = 1; // true
 
-    cout << 10 % 2 << endl;
-    cout << 9 % 2 << endl;
+    cout << "O valor eh: " << (c2 == true) << endl;//true ------> 1
+    cout << "O valor eh: " << (c2 == false) << endl;//false ------> 0
 
     system("pause");
     return 0;
