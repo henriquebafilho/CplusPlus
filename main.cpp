81a346e881e297b233a3225b1193867ad4851200
Henrique Filho <hbaf71@gmail.com>
2017-01-04 01:01:09 -0200

    Operadores de Incremento e Decremento
    x++, ++x, x-- e --x

--- main.cpp
+++ main.cpp
@@ -5,10 +5,25 @@ using namespace std;
 
 int main()
 {
-    for(int i = 0; i <= 10; i++)
-    {
-        cout << "o valor de i eh igual a: " << i << endl;
-    }
+    //Incremento ----> x++ = (x = x + 1)
+    //Decremento ----> x-- = (x = x - 1)
+
+    cout << "Operador de incremento" << endl;
+    cout << endl;
+    int i = 0, i2 = 0;
+    cout << "Pre " << ++i << endl;//pre-incrementar
+    cout << "Pos " << i2++ << endl;//pos-incrementar
+    cout << "Pos " << i2 << endl;
+
+    cout << endl;
+
+    cout << "Operador de decremento" << endl;
+    cout << endl;
+    i = 0;
+    i2 = 0;
+    cout << "Pre " << --i << endl;//pre-decrementar
+    cout << "Pos " << i2-- << endl;//pos-decrementar
+    cout << "Pos " << i2 << endl;
 
     system("pause");
     return 0;
