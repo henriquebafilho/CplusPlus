a2c96e0cc4a3f39e48f46cb3d6eead78aab08779
Henrique Filho <hbaf71@gmail.com>
2017-01-04 18:35:09 -0200

    Looping

--- main.cpp
+++ main.cpp
@@ -5,24 +5,13 @@ using namespace std;
 
 int main()
 {
-    //Imprimindo numeros de 0 a 99
-   int num = 0;
+    int i = 0;
 
-   while(num < 100)
-   {
-       cout << num << endl;
-       num++;
-   }
-    cout << endl;
-
-   //Imprimindo numeros de 99 a 0
-   int num2 = 100;
-
-   while(num2 > 0)
-   {
-       cout << num2 << endl;
-       num2--;
-   }
+    while(i<=0)
+    {
+        cout << i << endl;
+        i--;
+    }
 
     system("pause");
     return 0;
