689a7a9f054d45bdc636f2a54a5b4544b91200b4
Henrique Filho <hbaf71@gmail.com>
2017-01-05 01:12:42 -0200

    Switch

--- main.cpp
+++ main.cpp
@@ -5,23 +5,40 @@ using namespace std;
 
 int main()
 {
-    int i = 10;
-    cout << "Looping do-while" << endl;
-    do
-    {
-        i++;
-        cout << "O valor da variavel i eh: " << i << endl;
-    }
-    while(i>=10 && i <= 20);
+    int num = 0;
 
-    cout << "Looping while" << endl;
-    int i2 = 10;
-    while (i2 >= 10 && i2 <= 20)
-    {
-        i2++;
-        cout << "O valor da variavel i2 eh: " << i2 << endl;
-    }
+    cout << "Insira um numero correspondente a um mes desejado: " << endl;
+    cin >> num;
 
+    switch(num)
+    {
+        case 1: cout << "Janeiro" << endl;
+        break;
+        case 2: cout << "Fevereiro" << endl;
+        break;
+        case 3: cout << "Março" << endl;
+        break;
+        case 4: cout << "Abril" << endl;
+        break;
+        case 5: cout << "Maio" << endl;
+        break;
+        case 6: cout << "Junho" << endl;
+        break;
+        case 7: cout << "Julho" << endl;
+        break;
+        case 8: cout << "Agosto" << endl;
+        break;
+        case 9: cout << "Setembro" << endl;
+        break;
+        case 10: cout << "Outubro" << endl;
+        break;
+        case 11: cout << "Novembro" << endl;
+        break;
+        case 12: cout << "Dezembro" << endl;
+        break;
+        default : cout << "O valor inserido nao corresponde a nenhum mes" << endl;
+        break;
+    }
     system("pause");
     return 0;
 }
