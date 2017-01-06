b5438d24cde6ee4665d0fed697a831308261da8b
Henrique Filho <hbaf71@gmail.com>
2017-01-05 23:53:26 -0200

    Checar se o número é par ou ímpar

--- README.md
+++
@@ -1,2 +0,0 @@
-# Mexendo-com-c--
-Inserção de numeros entre 1 e 12 para descoberta de mes do ano
--- main.cpp
+++ main.cpp
@@ -6,40 +6,27 @@ using namespace std;
 
 int main()
 {
-   char c;
-   cout << "Por favor digite uma letra entre A e F: " << endl;
-   cin >> c;
+   int num = 0;
+   bool continuar = true;
 
-   switch(c)
+   while(continuar)
    {
-       case 'a':
-       case 'A':
-        cout << "Voce digitou a letra 'a' ou 'A'. " << endl;
-        break;
+       cout << "Insira um numero qualquer para saber se eh impar ou par: " << endl;
+       cin >> num;
 
-       case 'b':
-       case 'B':
-        cout << "Voce digitou a letra 'b' ou 'B'. " << endl;
-        break;
-
-        case 'c':
-        case 'C':
-        case 'd':
-        case 'D':
-        case 'e':
-        case 'E':
-            cout << "Voce digitou a letra 'c', 'C', 'd', 'D', 'e' ou 'E'. " << endl;
-            break;
-
-        case 'f':
-        case 'F':
-        cout << "Voce digitou a letra 'f' ou 'F'. " << endl;
-        break;
-
-        default: cout << "O valor digitado nao corresponde a nenhuma letra entre A e G. " << endl;
-        break;
+       while(num != 0 && num != 1)
+       {
+           num -=2;
+       }
+       if(num == 1)
+       {
+           cout << "o numero digitado eh impar" << endl;
+       }
+       else if(num == 0)
+       {
+           cout << "o numero digitado eh par" << endl;
+       }
    }
-
     system("pause");
     return 0;
 }
