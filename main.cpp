79e32364e0a6e8214f00699a2608163157aa275b
Henrique Filho <hbaf71@gmail.com>
2017-01-12 18:34:19 -0200

    Operação Ternário
    Simplificação de Código (If Else)
    FICA MANEIRÃO

--- main.cpp
+++ main.cpp
@@ -6,41 +6,13 @@ using namespace std;
 
 int main()
 {
-    cout << "Digite um numero entre 1 e 3" << endl;
-    int num = 0;
-    cin >> num;
+    cout << "Digite um numero: " << endl;
+    int i = 0;
+    cin >> i;
 
-    if(num == 1){
-        for(int i = 0; i < 5; i++)
-        {
-            cout << "A variavel da instrucao FOR eh " << i << endl;
-        }
-    }
-    else
-    {
-        if(num == 2)
-        {
-            int i2 = 0;
-            while(i2 < 5)
-            {
-                i2++;
-                cout << "O valor da variavel WHILE eh " << i2 << endl;
-            }
-        }
-        else
-        {
-            if(num == 3)
-            {
-                int i3 = 0;
-                do
-                {
-                    i3++;
-                    cout << "O valor da variavel DOWHILE eh " << i3 << endl;
-                }
-                while(i3 < 5);
-            }
-        }
-    }
+    string texto = (i <= 10) ? "menor que 10" : "maior que 10";
+
+    cout << "O numero inserido eh " << texto << endl;
 
     system("pause");
     return 0;
