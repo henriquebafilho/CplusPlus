6969e86a45477c94bf74263908c9fbd72c912402
Henrique Filho <hbaf71@gmail.com>
2017-01-13 16:44:54 -0200

    Associatividade de Operadores

--- main.cpp
+++ main.cpp
@@ -5,15 +5,18 @@ using namespace std;
 
 int main()
 {
-    int a, b, c;
-    a = 15;
-    b = 10;
-    c = 5;
+    int y = 0;
+    y = 3 * 3 + (2 + 2); //(1)
+    y = 3 * 3 + 4; //(2)
+    y = 9 + 4; //(3)
+    y = 13;//(4)
 
-    cout <<
-    ((((((((b+c)+1+((((((((((1000*10))))))))))))))))/1000/10-1)
-    <<
-    endl;
+    int a, b, c;
+    a=1, b=2, c=3;
+
+    y+=(a+=(b+=(c+=1)));
+
+    cout << y << endl;
 
     system("pause");
     return 0;
