0a728c61f4e41c8e9e723525c66befca52c78362
Henrique Filho <hbaf71@gmail.com>
2017-01-13 15:32:01 -0200

    Continue

--- main.cpp
+++ main.cpp
@@ -9,11 +9,18 @@ int main()
     while (true)
     {
         n++;
-        cout << n << endl;
         if(n >= 10)
         {
             break;
         }
+        else
+        {
+            if(n == 4 or n == 6)
+            {
+                continue;
+            }
+        }
+        cout << n << endl;
     }
 
     system("pause");
