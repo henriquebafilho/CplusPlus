41c292e53f70e5151c5286589fc9555bd242a9ba
Henrique Filho <hbaf71@gmail.com>
2017-02-07 16:40:20 -0200

    Scrobbler Last.fm

--- main.cpp
+++ main.cpp
@@ -5,18 +5,27 @@ using namespace std;
 
 int main()
 {
-    int tabela[2][2];
-    tabela[0][0] = 10;
-    tabela[0][1] = 100; //10, 100
-    tabela[1][0] = 20; //20, 111
-    tabela[1][1] = 111;
+    int minutos;
+    int segundos;
+    bool resultado = true;
+    bool continuar = true;
 
-    int tabela2[2][2] = {{10, 100},
-                         {20, 111}};
+    while(continuar){
+    cout << "Insira o numero de minutos da musica: " << endl;
+    cin >> minutos;
 
-    cout << "{{" << tabela2[0][0] << ", " << tabela2[0][1] << "}, {"
-                 << tabela2[1][0] << ", " << tabela2[1][1] << "}}" << endl;
-
+    cout << "Insira o numero de segundos da musica: " << endl;
+    cin >> segundos;
+    if(segundos >= 60)
+    {
+        resultado = false;
+        cout << "Operacao impossivel" << endl;
+    }
+    if(resultado == true){
+        cout << "A musica fara scrobble em: " << minutos / 2 << " minutos e "
+        << segundos / 2 << " segundos." << endl;
+        }
+    }
     system("pause");
     return 0;
 }
