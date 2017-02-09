21069c052dc319630e44fc72c24b46910c6a3813
Henrique Filho <hbaf71@gmail.com>
2017-02-09 21:13:25 -0200

    Scrobbler Last.Fm Final 1.1

--- main.cpp
+++ main.cpp
@@ -8,38 +8,38 @@ int main(){
     int segundos;
     bool resultado = true;
     bool continuar = true;
-    bool maiorqueoito = true;
+    bool maiorqueoito = false;
     bool minpar;
 
     while(continuar){
-    cout << "Insira o numero de minutos da musica: " << endl;
-    cin >> minutos;
-    if(minutos % 2 == 0) { minpar = true; }
-    else { minpar = false; }
+        cout << "Insira o numero de minutos da musica: " << endl;
+        cin >> minutos;
+        if(minutos % 2 == 0) { minpar = true; }
+        else { minpar = false; }
 
-    cout << "Insira o numero de segundos da musica: " << endl;
-    cin >> segundos;
-
-    if(segundos >= 60){
-        resultado = false;
-        cout << "Operacao impossivel" << endl;
-    }
-    if(resultado == true){
-            if(minutos >= 8){
-                cout << " A musica fara scrobble em 4 minutos e 0 segundos." << endl;
-                maiorqueoito = true;
-            }
-            if(maiorqueoito == false){
-                if(minpar == true){
-                    cout << "A musica fara scrobble em " << minutos / 2 << " minutos e " <<
-                    segundos / 2 << " segundos." << endl;
-                }
-                if(minpar == false){
-                    cout << " A musica fara scrobble em " << minutos / 2 << " minutos e " <<
-                    (segundos + 60) / 2 << " segundos." << endl;
-                }
-            }
+        cout << "Insira o numero de segundos da musica: " << endl;
+        cin >> segundos;
+        if(segundos >= 60){
+            resultado = false;
+            cout << "Operacao impossivel" << endl;
         }
+
+        if(resultado == true){
+                if(minutos >= 8){
+                    cout << " A musica fara scrobble em 4 minutos e 0 segundos." << endl;
+                    maiorqueoito = true;
+                }
+                if(maiorqueoito == false){
+                    if(minpar == true){
+                        cout << "A musica fara scrobble em " << minutos / 2 << " minutos e " <<
+                        segundos / 2 << " segundos." << endl;
+                    }
+                    if(minpar == false){
+                        cout << " A musica fara scrobble em " << minutos / 2 << " minutos e " <<
+                        (segundos + 60) / 2 << " segundos." << endl;
+                    }
+                }
+            }
     }
     system("pause");
     return 0;
