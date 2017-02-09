51cdbb3225e47a6e543bfed0d68a328bcefc3542
Henrique Filho <hbaf71@gmail.com>
2017-02-09 21:08:00 -0200

    Scrobbler Last.Fm Final

--- main.cpp
+++ main.cpp
@@ -3,33 +3,42 @@
 #include <iomanip>
 using namespace std;
 
-int main()
-{
+int main(){
     int minutos;
     int segundos;
     bool resultado = true;
     bool continuar = true;
+    bool maiorqueoito = true;
+    bool minpar;
 
     while(continuar){
     cout << "Insira o numero de minutos da musica: " << endl;
     cin >> minutos;
+    if(minutos % 2 == 0) { minpar = true; }
+    else { minpar = false; }
 
     cout << "Insira o numero de segundos da musica: " << endl;
     cin >> segundos;
-    if(segundos >= 60)
-    {
+
+    if(segundos >= 60){
         resultado = false;
         cout << "Operacao impossivel" << endl;
     }
-
     if(resultado == true){
-            if(minutos >= 8)
-            {
-                minutos = 8;
-                segundos = 0;
+            if(minutos >= 8){
+                cout << " A musica fara scrobble em 4 minutos e 0 segundos." << endl;
+                maiorqueoito = true;
             }
-        cout << "A musica fara scrobble em: " << minutos / 2 << " minutos e "
-        << segundos / 2 << " segundos." << endl;
+            if(maiorqueoito == false){
+                if(minpar == true){
+                    cout << "A musica fara scrobble em " << minutos / 2 << " minutos e " <<
+                    segundos / 2 << " segundos." << endl;
+                }
+                if(minpar == false){
+                    cout << " A musica fara scrobble em " << minutos / 2 << " minutos e " <<
+                    (segundos + 60) / 2 << " segundos." << endl;
+                }
+            }
         }
     }
     system("pause");
